fef1007f2620aac2963dc7d61ffeaf9472c7665a
Matvey Vysotskiy <ma.visotskiy@gmail.com>
2013-05-15 15:51:12 +0400

    chg: removed comments

--- Strategy/StrategyResolver.hpp
+++ Strategy/StrategyResolver.hpp
@@ -19,9 +19,9 @@ class CStrategyResolver {
 
 public:
 	~CStrategyResolver() {
-//		for (uint32_t i = 0; i < strategies.size(); i++) {
-//			delete strategies[i];
-//		}
+		for (uint32_t i = 0; i < strategies.size(); i++) {
+			delete strategies[i];
+		}
 	}
 	static CStrategyResolver& getInstance() {
 		static CStrategyResolver theSingleInstance;
@@ -29,27 +29,30 @@ public:
 	}
 
 	void init(string str) {
-//		int i = 0;
-//		uint32_t pos = str.find(getStrategiesNames(i) + "=\"", 0);
-//		string::iterator it = str.begin();
-//		it += pos + getStrategiesNames(i).size() + 2;
-//		string::iterator start = it;
-//		stack<char> st;
-//		string result;
-//		while (it != str.end()) {
-//			string::iterator next = it + 1;
-//			if ((*it) == '=' && (*next) == '\"') {
-//				st.push('"');
-//			} else if ((*it) == '\"') {
-//				if (st.empty()) {
-//					result = string(start, it);
-//					break;
-//				} else {
-//					st.pop();
-//				}
-//			}
-//		}
-//		cout << *it << endl << endl;
+		for (int i = 0; i < strategies.size(); i++) {
+			uint32_t pos = str.find(getStrategiesNames(i) + "=\"", 0);
+			string::iterator it = str.begin();
+			it += pos + getStrategiesNames(i).size() + 2;
+			string::iterator start = it;
+			stack<char> st;
+			string result = "";
+			while (it != str.end()) {
+				string::iterator next = it + 1;
+				if ((*it) == '=' && (*next) == '\"') {
+					st.push('"');
+					it++;
+				} else if ((*it) == '\"') {
+					if (st.empty()) {
+						result = string(start, it);
+						break;
+					} else {
+						st.pop();
+					}
+				}
+				it++;
+			}
+			strategies[i]->setSettings(result);
+		}
 	}
 
 	const string& getStrategiesNames(uint32_t index) const {
@@ -59,12 +62,12 @@ public:
 
 private:
 	CStrategyResolver() {
-//		strategies.push_back(new CPTMStrategy());
+		strategies.push_back(new CPTMStrategy());
 	}
 	CStrategyResolver(const CStrategyResolver& root) {
 	}
 	CStrategyResolver& operator=(const CStrategyResolver&) {
 	}
 
-//	vector<CStrategy*> strategies;
+	vector<CStrategy*> strategies;
 };
