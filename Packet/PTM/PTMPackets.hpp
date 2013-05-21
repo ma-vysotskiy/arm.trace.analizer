c9fc014831ab9b2c014a7203af8731a245c443ab
Matvey Vysotskiy <ma.visotskiy@gmail.com>
2013-05-21 19:00:38 +0400

    add: PTMPackets header

---
+++ Packet/PTM/PTMPackets.hpp
@@ -0,0 +1,17 @@
+/*
+ * PTMPackets.hpp
+ *
+ *  Created on: May 20, 2013
+ *      Author: mvis
+ */
+
+#include "../../Packet/PTM/PTMAsyncPacket.hpp"
+#include "../../Packet/PTM/PTMBranchPacket.hpp"
+#include "../../Packet/PTM/PTMExceptionReturnPacket.hpp"
+#include "../../Packet/PTM/PTMAtomPacket.hpp"
+#include "../../Packet/PTM/PTMContextIDPacket.hpp"
+#include "../../Packet/PTM/PTMIgnorePacket.hpp"
+#include "../../Packet/PTM/PTMIsyncPacket.hpp"
+#include "../../Packet/PTM/PTMTimestampPacket.hpp"
+#include "../../Packet/PTM/PTMTriggerPacket.hpp"
+#include "../../Packet/PTM/PTMWaypointPacket.hpp"
