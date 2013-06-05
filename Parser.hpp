03863595db579ebe8b3e3db020863540c727a671
Matvey Vysotskiy <ma.visotskiy@gmail.com>
2013-06-05 16:04:04 +0400

    fix: parser broken frame decrypting

--- Parser.hpp
+++ Parser.hpp
@@ -32,82 +32,82 @@ public:
 		map<uint32_t, dataType> IDtoData;
 		dataType rawData = getRawData(is, mask);
 		uint32_t currID = 0;
-//		IDtoData.insert(make_pair(1, rawData));
 		uint32_t count = 0;
 		// count variable placed here for crutch ;)
-		try{
-		for (dataType::iterator it = rawData.begin(); (count + 16)<= rawData.size();
-				advance(it, 16)) {
-			count+= 16;
-			dataType::iterator lastByteIt = it;
-			advance(lastByteIt, 16);
-			if (lastByteIt == rawData.end()) {
-				throw notenough_data();
-			}
-//			CData& lastByte = *lastByteIt;
-			for (uint32_t i = 0; i < 15; i++) {
-				CData currByte = *(it + i);
-
-				uint8_t lastBit = (((*lastByteIt).data >> ((uint32_t) (i / 2)))
-						& 1);
-				bool isID = ((currByte.data) & 0x1) && (i % 2 == 0);
-				if (!i % 2) {
-					currByte.data = (currByte.data & (~1)) | lastBit;
+		try {
+			for (dataType::iterator it = rawData.begin();
+					(count + 16) <= rawData.size(); advance(it, 16)) {
+				count += 16;
+				dataType::iterator lastByteIt = it;
+				advance(lastByteIt, 15);
+				if (lastByteIt == rawData.end()) {
+					throw notenough_data();
 				}
-				if (isID) {
-					// ID
-					CData nextByte;
-					if (!lastBit) {
-						// lastBit == 0 new id takes effect immediately
-						currID = currByte.data >> 1;
-					} else {
-						// lastBit == 1 add next byte to old id
-						i++;
-						// next byte
-						nextByte = *(it + i);
-						// find old id
+				for (uint32_t i = 0; i < 15; i++) {
+					CData currByte = *(it + i);
+
+					uint8_t lastBit = (((*lastByteIt).data
+							>> ((uint32_t) (i / 2))) & 1);
+					bool isID = ((currByte.data) & 0x1) && (i % 2 == 0);
+					if (!(i % 2)) {
+						currByte.data = (currByte.data & (~1)) | lastBit;
+					}
+					if (isID) {
+						// ID
+						CData nextByte;
+						if (!lastBit) {
+							// lastBit == 0 new id takes effect immediately
+							currID = currByte.data >> 1;
+						} else {
+							// lastBit == 1 add next byte to old id
+							i++;
+							// next byte
+							nextByte = *(it + i);
+							// find old id
+							map<uint32_t, dataType>::iterator dataIt =
+									IDtoData.find(currID);
+							// found - add
+							if (dataIt != IDtoData.end()) {
+								dataIt->second.push_back(nextByte);
+							} else {
+								// if cannot find old id that is really bad
+								cout << "Error! Old ID " << nextByte.data
+										<< " was not found!" << endl;
+							}
+							// now new id takes effect
+							currID = currByte.data >> 1;
+						}
+						// find new id
 						map<uint32_t, dataType>::iterator dataIt =
 								IDtoData.find(currID);
-						// found - add
+						// found - changing to new id
 						if (dataIt != IDtoData.end()) {
-							dataIt->second.push_back(nextByte);
+							cout << "Info! Changing ID to " << currID << "!"
+									<< endl;
 						} else {
-							// if cannot find old id that is really bad
-							cout << "Error! Old ID " << nextByte.data
+							if (currID == 0) {
+								int k = 0;
+							}
+							// not found - add new id
+							cout << "Info! New ID is " << currID << "!" << endl;
+							IDtoData.insert(make_pair(currID, dataType()));
+						}
+					} else {
+						// DATA
+						map<uint32_t, dataType>::iterator dataIt =
+								IDtoData.find(currID);
+						if (dataIt != IDtoData.end()) {
+							dataIt->second.push_back(currByte);
+						} else {
+							cout << "Warning! Current ID " << currByte.data
 									<< " was not found!" << endl;
 						}
-						// now new id takes effect
-						currID = currByte.data >> 1;
-					}
-					// find new id
-					map<uint32_t, dataType>::iterator dataIt = IDtoData.find(
-							currID);
-					// found - changing to new id
-					if (dataIt != IDtoData.end()) {
-						cout << "Info! Changing ID to " << currID << "!"
-								<< endl;
-					} else {
-						if(currID == 0){
-							int k=0;
-						}
-						// not found - add new id
-						cout << "Info! New ID is " << currID << "!" << endl;
-						IDtoData.insert(make_pair(currID, dataType()));
-					}
-				} else {
-					// DATA
-					map<uint32_t, dataType>::iterator dataIt = IDtoData.find(
-							currID);
-					if (dataIt != IDtoData.end()) {
-						dataIt->second.push_back(currByte);
-					} else {
-						cout << "Warning! Current ID " << currByte.data
-								<< " was not found!" << endl;
 					}
 				}
 			}
+		} catch (notenough_data& e) {
+			cout << "Warning! " << e.what() << endl;
 		}
-		}catch(notenough_data& e){}
 		// output
 		for (map<uint32_t, dataType>::iterator it = IDtoData.begin();
 				it != IDtoData.end(); it++) {
@@ -159,15 +159,34 @@ private:
 			} else {
 //				throw 0;
 			}
-//				data.data = value & 0xff;
-//				data.ts = ts;
-//				data2.data = (value >> 8) & 0xff;
-//				data2.ts = ts;
-//				rawData.push_back(data);
-//				rawData.push_back(data2);
-
 		}
+		removeSyncData(rawData);
 		return rawData;
+	}
+
+	void removeSyncData(dataType& rawData) {
+		dataType::iterator it = rawData.begin();
+		while (it != rawData.end()) {
+			if (it->data == 0xff) {
+				uint32_t ffcount = 0;
+				dataType::iterator mem = it;
+				it++;
+				while ((it->data == 0xff) && (it != rawData.end())) {
+					ffcount++;
+					it++;
+				}
+				if ((it->data == 0x7f) && (it != rawData.end())) {
+					ffcount++;
+					it++;
+				}
+				if ((ffcount == 1) || (ffcount == 3)) {
+					it = rawData.erase(mem, it);
+				}
+			} else {
+				it++;
+			}
+		}
+
 	}
 
 	CParser() {
